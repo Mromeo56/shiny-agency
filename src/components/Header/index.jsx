d9ad0d103974e6c9194d202cac053cc6e9c895a7
Romeo <romeo.moreaux35@gmail.com>
2023-06-05 19:31:41 +0200

    styled header / styled in js

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,27 +1,42 @@
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import colors from '../../utils/style/colors.js'
+import darklogo from '../../assets/dark-logo.png'
 
 const StyledLink = styled(Link)`
-  padding: 15px;
+  padding: 10px 15px;
   color: #8186a0;
   text-decoration: none;
   font-size: 18px;
+  text-align: center;
   ${(props) =>
     props.$isFullLink &&
-    `color: white; border-radius: 30px; background-color: ${colors.primary};`}
+    `color: white; 
+    border-radius: 30px; 
+    background-color: ${colors.primary};`}
+`
+
+const StyledNav = styled.nav`
+  padding: 30px;
+  display: flex;
+  align-items: center;
+  flex-direction: row-reverse;
+  justify-content: space-between;
 `
 
 function Header() {
   return (
-    <nav>
-      <StyledLink to="/survey/1" $isFullLink>
-        Faire le test
-      </StyledLink>
-      <StyledLink to="/">Accueil</StyledLink>
-      <StyledLink to="/survey/1">Questionnaire</StyledLink>
-      <StyledLink to="/freelances">Profils</StyledLink>
-    </nav>
+    <StyledNav>
+      <div>
+        <StyledLink to="/survey/1">Questionnaire</StyledLink>
+        <StyledLink to="/">Accueil</StyledLink>
+        <StyledLink to="/freelances">Profils</StyledLink>
+        <StyledLink to="/survey/1" $isFullLink>
+          Faire le test
+        </StyledLink>
+      </div>
+      <img style={{ height: 70 }} alt="Shiny" src={darklogo} />
+    </StyledNav>
   )
 }
 
